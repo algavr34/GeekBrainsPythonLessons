069e2254b01056196fbe94c82083e8122ac3ca95
Aleksey <algavr34@live.com>
2020-10-06 16:11:12 +0300

    Все задания по уроку 1 выполнены. С заданием 2 возникли трудности, хотел бы разобраться подробнее

---
+++ task3.py
@@ -0,0 +1,9 @@
+print("Задание 3")
+n = str(input("Введите цифру n: "))
+a = n * 2
+a = int(a)
+b = n * 3
+b = int(b)
+n = int(n)
+print("Получились числа ", n, a, b)
+print("Сумма n + nn + nnn равна: ", (n + a + b))
---
+++ task6.py
@@ -0,0 +1,9 @@
+print("Задание 6")
+first = int(input("Введите, сколько вы пробежали в первый день: "))
+maximum = int(input("Введите, сколько вы хотите пробегать: "))
+days = 1
+while maximum >= first:
+    print("День", days, ":", first)
+    first = first * 1.1
+    days = days + 1
+print(f"На {days} день вы достигнете показателя в {first} км")
