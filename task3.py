2c15608405883506fc45ac7694423b72e6fb8b18
algavr34 <72412435+algavr34@users.noreply.github.com>
2020-10-06 20:39:04 +0300

    Delete task3.py

--- task3.py
+++
@@ -1,9 +0,0 @@
-print("Задание 3")
-n = str(input("Введите цифру n: "))
-a = n * 2
-a = int(a)
-b = n * 3
-b = int(b)
-n = int(n)
-print("Получились числа ", n, a, b)
-print("Сумма n + nn + nnn равна: ", (n + a + b))
