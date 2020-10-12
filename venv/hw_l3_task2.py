ec2882ada16d71b7d8794220ead9b6e7c242132a
Aleksey <algavr34@live.com>
2020-10-12 17:27:23 +0300

    Второе выполнено!

---
+++ venv/hw_l3_task2.py
@@ -0,0 +1,4 @@
+def user_info(name, surname, birthyear, email, number):
+    print(f"Name - {name}, Surname - {surname}, year of birth - {birthyear}, email - {email}, number - {number}")
+
+print(user_info("Alex", "Gavrilov", 1994, "algavrrf@gmail.com", "8-999-999-99-99"))
